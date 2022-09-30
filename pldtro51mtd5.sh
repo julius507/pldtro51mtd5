580c84830b6c8137f3024e124c82f7983c0007bc
terence <99847498+julius507@users.noreply.github.com>
2022-09-30 17:54:10 +0800

    Delete pldtro51mtd5.sh

--- pldtro51mtd5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "Starting..."
-ubus call version set_sn_imei_info '{"imei":"0"}'
-
-wget http://github.com/julius507/ro51pldt/raw/master/r051pldtjac.bin -O /tmp/firmware.bin
-
-mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2&>1
